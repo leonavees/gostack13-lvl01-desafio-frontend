5d6dcb11cac62c644a5de6158080e32ded3224b9
Leonardo <Leonardo@DESKTOP-02UL904>
2020-08-21 10:28:50 -0300

    Desafio front-end completo

--- src/App.js
+++ src/App.js
@@ -1,31 +1,62 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
-  async function handleAddRepository() {
-    // TODO
-  }
+    const [repositories, setRepositories] = useState([]);
 
-  async function handleRemoveRepository(id) {
-    // TODO
-  }
+    async function handleAddRepository() {
+        const response = await api.post('repositories', {
+            title: `Título${Date.now()}`,
+            url: "",
+            techs: ['Node.js', 'ReactJS', 'React Native']
+        })
 
-  return (
-    <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        setRepositories([...repositories, response.data]);
+    }
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
-      </ul>
+    async function handleRemoveRepository(id) {
+        try {
+            await api.delete(`repositories/${id}`)
 
-      <button onClick={handleAddRepository}>Adicionar</button>
-    </div>
-  );
+            setRepositories([...repositories].filter(repository => repository.id !== id));
+        }catch (err){
+            console.log(err);
+            alert('Não foi possível remover repositório. Tente novamente mais tarde!')
+        }
+    }
+
+    useEffect(() => {
+        async function loadRepositories(){
+            const response = await api.get('repositories');
+
+            setRepositories(response.data);
+        }
+
+        loadRepositories();
+        
+    }, [])
+
+    return (
+        <div>
+            <ul data-testid="repository-list">
+                {repositories.map(repository => (
+                    <li key={repository.id}>
+                        {repository.title}
+
+                        <button onClick={() => handleRemoveRepository(repository.id)}>
+                            Remover
+                        </button>
+                    </li>
+                ))}
+                
+            </ul>
+
+            <button onClick={handleAddRepository}>Adicionar</button>
+        </div>
+    );
 }
 
 export default App;
